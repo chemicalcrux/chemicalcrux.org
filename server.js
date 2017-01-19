c48f3c99d6f83e805537ac2a186df098c2f87730
Etherealflux <haussmsg@rose-hulman.edu>
2017-01-19 14:33:24 -0500

    Dumping existing files into the repo

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+  , logger = require('morgan')
+  , app = express()
+
+app.use(logger('dev'))
+app.use(express.static(__dirname + '/static'))
+app.set('view engine', 'pug')
+app.set('views', './source/templates')
+
+app.get('/', function (req, res, next) {
+  res.render('homepage')
+})
+
+app.get('/about', function (req, res, next) {
+  res.render('about')
+})
+
+app.get('/interests', function(req, res, next) {
+  res.render('interests')
+})
+
+app.get('/work', function (req, res, next) {
+  res.render('work')
+})
+
+app.get('/art', function (req, res, next) {
+  res.render('art')
+})
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
