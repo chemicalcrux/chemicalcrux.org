c1c9e42da726c8efc630301b104aa5bf931cb3ba
Fen Dweller <chemicalcrux@gmail.com>
2018-03-05 16:56:08 -0500

    Trying to fix CSP

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ function setHeaders(res, path, stat) {
   res.set('X-XSS-Protection','1; mode=block');
   res.set('X-Content-Type-Options','nosniff');
   res.set('Referrer-Policy','strict-origin');
-  res.set('Content-Security-Policy',"default-src 'none'; font-src https://fonts.gstatic.com; img-src 'self'; object-src 'none'; script-src 'self'; style-src 'self' https://fonts.googleapis.com")
+  res.set('Content-Security-Policy',"default-src 'none'; font-src https://fonts.gstatic.com; img-src 'self'; object-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com")
 }
 
 var options = {
