e8a62a2c2235aa32fd07ea29b8cf0a165fa9cc65
Fen Dweller <chemicalcrux.org>
2019-01-04 20:26:06 -0600

    Added Satiate media folder

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use('/nightly/stroll', express.static(__dirname + '/nightly/stroll', options
 app.use('/gorge', express.static(__dirname + '/gorge', options));
 app.use('/nightly/gorge', express.static(__dirname + '/nightly/gorge', options));
 app.use('/nightly/satiate', express.static(__dirname + '/nightly/satiate', options));
+app.use('/satiate/media', express.static(__dirname + '/satiate/media', options));
 app.use('/preview', express.static(__dirname + '/preview', options));
 app.use('/april', express.static(__dirname + '/april', options));
 
